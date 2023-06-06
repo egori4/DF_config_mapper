e3bdd909bccc304f892434256e10fe3ea9b95dc9
egori4 <egori4@gmail.com>
2023-06-06 13:39:52 -0400

    V1.0

--- df_config_mapper.py
+++ df_config_mapper.py
@@ -4,26 +4,45 @@
 import json
 import csv
 import sys
+import os
 
 # input file
-json_file = sys.argv[1]
-# json_file = 'protected_object_configuration.json'
+
+df_config_json_file_directory = "DF_config_input"
+
+# Get the absolute path to the directory
+df_config_json_dir_path = os.path.abspath(df_config_json_file_directory)
+
+# List all files in the directory
+df_config_json_files = os.listdir(df_config_json_dir_path)
+
+# Check if there are any files in the directory
+if len(df_config_json_files) > 0:
+    for f in df_config_json_files:
+        # Check if the file's extension is one of the supported types
+        if f != "dummy_file" and f.endswith(".json"):
+    # Open the first file in the directory
+            df_config_json_file_name = f  # Change index if you want to open a different file
+            df_config_json_file_path = os.path.join(df_config_json_dir_path, df_config_json_file_name)
+
+
 
 # output file
-csv_file = sys.argv[2]
-# csv_file = 'protected_object_configuration.csv'
+csv_file_path = "./DF_mapping_output/protected_object_configuration.csv"
 
-# open json file
-with open(json_file) as f:
+
+with open(df_config_json_file_path) as f:
     data = json.load(f)
     
 json_dic = data['protectedObjects']
 # print(json_dic)
+
 # open csv file
-csvfile = open(csv_file, 'w')
+
+csv_file = open(csv_file_path, 'w')
 
 # create csv writer
-csvwriter = csv.writer(csvfile)
+csvwriter = csv.writer(csv_file)
 
 # write header
 
@@ -35,5 +54,5 @@ for row in json_dic:
 	
 # close files
 f.close()
-csvfile.close()
+csv_file.close()
 
